72b8a9f9b23b6cc1845ee2a24a0c4e9f55fdadaa
Nikolas Santis <nikosantis@gmail.com>
2020-02-18 22:25:19 -0300

    Props and render types

--- src/App.js
+++ src/App.js
@@ -9,16 +9,67 @@ const Productos = () => (
   <h1>Productos</h1>
 )
 
+const Home = () => (
+  <h1>Home</h1>
+)
+
+const HolaNinja = () => (
+  <h1>Hola Ninja</h1>
+)
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path='/hola' component={Hola} />
-      <section>
-        <Route path='/productos' component={Productos} />
-      </section>
-      <div>
-        <Route path='/productos' component={Productos} />
-      </div>
+      <Route
+        path='/'
+        exact
+        component={Home}
+      />
+      <Route
+        path='/hola'
+        exact
+        sensitive
+        render={Hola}
+      />
+      <Route
+        path='/hola/ninja'
+        exact
+        component={HolaNinja}
+      />
+      <Route
+        path='/arrow'
+        exact
+        component={() => (<h1>Testendo Arrow Function en Router</h1>)}
+      />
+      <Route
+        path='/productos'
+        exact
+        strict
+        component={Productos}
+      />
+      <Route
+        path='/productos2'
+        exact
+      >
+        <Productos />
+      </Route>
+      <Route
+        path='/productos3'
+        exact
+      >
+        {({ match }) => {
+          if (!match) {
+            return (
+              <div>
+                *** woops no coincide con productos
+              </div>
+            )
+          }
+          return (
+            <Productos />
+          )
+        }}
+      </Route>
     </BrowserRouter>
   )
 }
