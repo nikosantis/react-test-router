2727da46abc1bea5d638e97edfaa4799af5099cd
Nikolas Santis <nikosantis@gmail.com>
2020-02-19 16:06:28 -0300

    NavLinks, Props Route, Params

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom'
+
+import './App.css'
 
 const Hola = () => (
   <h1>Hola</h1>
 )
 
 const Productos = () => (
-  <h1>Productos</h1>
+  <div>
+    <h1>Productos</h1>
+    <Link to='/productos/empresa1'>Empresa 1</Link>
+    <Link to='/productos/empresa2'>Empresa 2</Link>
+  </div>
 )
 
-const Home = () => (
-  <h1>Home</h1>
-)
+const Home = (props) => {
+  return (
+    <h1>Home</h1>
+  )
+}
 
 const HolaNinja = () => (
   <h1>Hola Ninja</h1>
 )
 
+const navStyles = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
+
+const NavActive = {
+  color: 'orangered'
+}
+
+const Navigation = () => (
+  <nav style={navStyles}>
+    <NavLink
+      to='/'
+      exact
+      activeStyle={NavActive}
+    >
+      Home
+    </NavLink>
+    <NavLink
+      to='/hola'
+      activeClassName='navActive'
+    >
+    Hola
+    </NavLink>
+    <NavLink
+      to='/productos'
+      activeStyle={NavActive}
+    >
+      Productos
+    </NavLink>
+  </nav>
+)
+
+const ProductosCategoria = ({ match }) => {
+  console.log(match)
+  return (
+    <div>
+      <h1>Categoría: {match.params.categoria}</h1>
+      {
+        match.params.id
+          ? (
+            <h2>id: {match.params.id}</h2>
+          )
+          : ''
+      }
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <BrowserRouter>
+      <Navigation />
       <Route
         path='/'
         exact
-        component={Home}
+        render={Home}
       />
       <Route
         path='/hola'
@@ -34,42 +92,17 @@ const App = () => {
       <Route
         path='/hola/ninja'
         exact
-        component={HolaNinja}
-      />
-      <Route
-        path='/arrow'
-        exact
-        component={() => (<h1>Testendo Arrow Function en Router</h1>)}
+        render={HolaNinja}
       />
       <Route
         path='/productos'
         exact
-        strict
-        component={Productos}
+        render={Productos}
       />
       <Route
-        path='/productos2'
-        exact
-      >
-        <Productos />
-      </Route>
-      <Route
-        path='/productos3'
-        exact
-      >
-        {({ match }) => {
-          if (!match) {
-            return (
-              <div>
-                *** woops no coincide con productos
-              </div>
-            )
-          }
-          return (
-            <Productos />
-          )
-        }}
-      </Route>
+        path='/productos/:categoria/:id?'
+        render={ProductosCategoria}
+      />
     </BrowserRouter>
   )
 }
